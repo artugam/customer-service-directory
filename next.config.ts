07450581e662f6b8b8f65bee5755f6dc0a2bd790
karolina011 <karolina.pytlak11@gmail.com>
2025-12-16 11:20:58 +0100

    feat add some platforms and fix errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "logo.svgcdn.com" },
+      { protocol: "https", hostname: "www.logowik.com" },
+      { protocol: "https", hostname: "www.zohowebstatic.com" },
+      { protocol: "https", hostname: "www.servicenow.com" },
+      { protocol: "https", hostname: "www.hubspotware.net" },
+      { protocol: "https", hostname: "commons.wikimedia.org" },
+      { protocol: "https", hostname: "brandeps.com" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+      { protocol: "https", hostname: "images.g2crowd.com" },
+    ],
+  },
 };
 
 export default nextConfig;
